b7fc5a5fdca0ca6e5a48623ca8c09794029f6cf1
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-07-06 12:53:46 -0500

    Project setup, development environment ready.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+import App from './components/App'
+
+const app = ReactDOM.createRoot(document.getElementById('app'))
+
+app.render(
+    <App />
+)
