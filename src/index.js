a10255ac4fdcf5c21cf19ca281773de547da7366
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-07-07 16:08:19 -0500

    Added router and routes to project.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { App } from './components/App'
+import { App } from './routes/App'
 
 const app = ReactDOM.createRoot(document.getElementById('app'))
 
