084460227b16513def6f331dd6f0cd8870820fba
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-07-07 16:33:02 -0500

    Added style to the site.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/components/Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+        <p className='Footer-title'>Reactive-Commerce</p>
+        <p className='Footer-copy'>All Rights and Lefts Reserved</p>
+    </div>
+  )
+}
+
+export { Footer }
